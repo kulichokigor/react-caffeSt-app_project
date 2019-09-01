719cf3caa414e0c1a8b142bca95905ba53ad6333
kulichokigor <igorkulichok@gmail.com>
2019-09-01 17:05:41 +0300

    start_proj_commit-01.09.19

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../src/css/App.css';
+import 'normalize.css';
+import logo from '../src/img/logo.png';
+import Slider from '../src/components/slider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faViber, faInstagram } from '@fortawesome/free-brands-svg-icons'
+// import { faAngry} from '@fortawesome/free-solid-svg-icons'
+
+class App extends React.Component{
+ constructor(props){
+   super(props);
+
+ }
+  render(){
+    return(
+      <div className="site-body">
+        <header>
+          <div className="conteiner">
+            <div className="top-header">
+              <div className="main-logo pos-rel">
+                <img src={logo} width="36" height="36" alt="Our logo"/>
+              </div>
+              <nav>
+                <ul>
+                  <li>About</li>
+                  <li>Menu</li>
+                  <li>Gallery</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            </div>
+            <div>
+                <Slider/>
+            </div>
+          </div>
+        </header>
+        <main>
+          <div className="conteiner">
+            <section>
+              <div className="section-main-logo">
+                <h2>вдосконалення</h2>
+                <span>Музика під час обіду - це образа і для кухаря, і для скрипаля</span>
+              </div>
+            </section>
+            <section>
+                <div className="section-main-item">
+                  <div>
+                    <p>desserts</p>
+                    <p><hr color='#d89b64'/></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                  <div>
+                    <p>smothies</p>
+                    <p><hr color='#151b29'/></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                  <div>
+                    <p>breackfast</p>
+                    <p><hr color='#d89b64'/></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                </div>
+            </section>
+            <section>
+              <h3>check other offers</h3>
+              <div className="details-box">
+                <div>
+                  <div>
+                    <p>details</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <input type="button" value="preparation of our dishes"/>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section>
+              <div className="section-main-logo">
+              <h2>the third block</h2>
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+              </div>
+              <div className="third-section-box">
+                <div>
+                      <p>diffrent locations</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div>
+                      <p>fresh products</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div>
+                      <p>the best from the beggining</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+              </div>
+            </section>
+          </div>
+        </main>
+        <footer>
+          <div className="conteiner pos-rel">
+            <div className="footer-logo-box">
+              <img src={logo} alt="Buffet Cafe logo"/>
+            </div>
+            <div className="copyright">
+              <p>Buffet Cafe</p>
+              <p>Copyright 2019 MrJingls. All rights reserved</p></div>
+            <div className="social-footer-box">
+              <FontAwesomeIcon icon={faFacebookSquare} size="2x" className="social-facebook"/>
+              <FontAwesomeIcon icon={faViber} size="2x" className="social-viber"/>
+              <FontAwesomeIcon icon={faInstagram} size="2x" className="social-insta"/>
+              </div>
+          </div>
+        </footer>
+      </div>
+    )
+  }
 }
 
 export default App;
