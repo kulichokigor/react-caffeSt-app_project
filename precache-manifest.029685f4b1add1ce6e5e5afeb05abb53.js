6b1b23fa1936c42c54d4a9d62abd4f83e786479b
kulichokigor <igorkulichok@gmail.com>
2019-09-01 18:22:24 +0300

    Updates

---
+++ precache-manifest.029685f4b1add1ce6e5e5afeb05abb53.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1244bf2e87bfe3da457e7d3dec13653d",
+    "url": "/react-caffeSt-app_project/index.html"
+  },
+  {
+    "revision": "e0fe01ad688d08bfd5cb",
+    "url": "/react-caffeSt-app_project/static/css/2.db5b1de2.chunk.css"
+  },
+  {
+    "revision": "193df58400e33cfbf0ac",
+    "url": "/react-caffeSt-app_project/static/css/main.6d70fd17.chunk.css"
+  },
+  {
+    "revision": "e0fe01ad688d08bfd5cb",
+    "url": "/react-caffeSt-app_project/static/js/2.ecc25d5f.chunk.js"
+  },
+  {
+    "revision": "193df58400e33cfbf0ac",
+    "url": "/react-caffeSt-app_project/static/js/main.4c62cd66.chunk.js"
+  },
+  {
+    "revision": "a1e3acaf60dba43130b5",
+    "url": "/react-caffeSt-app_project/static/js/runtime~main.9ae12ecf.js"
+  },
+  {
+    "revision": "7d67eeae1303863d0bf1bc9369811d20",
+    "url": "/react-caffeSt-app_project/static/media/banner-1.7d67eeae.jpg"
+  },
+  {
+    "revision": "d2c9e3287e24989bf58eaa92ea1c2e1b",
+    "url": "/react-caffeSt-app_project/static/media/banner-3.d2c9e328.jpg"
+  },
+  {
+    "revision": "fcad699f576718c4f22769f7b1251070",
+    "url": "/react-caffeSt-app_project/static/media/breackfast.fcad699f.jpg"
+  },
+  {
+    "revision": "e9c9a193461d4094682944808e012446",
+    "url": "/react-caffeSt-app_project/static/media/buffet-image2.e9c9a193.jpg"
+  },
+  {
+    "revision": "552769db1cd39d99df8d40ed0506bba7",
+    "url": "/react-caffeSt-app_project/static/media/dessert.552769db.jpg"
+  },
+  {
+    "revision": "ea6de923e101e6e80540a8512db932f8",
+    "url": "/react-caffeSt-app_project/static/media/order_backgr.ea6de923.jpg"
+  },
+  {
+    "revision": "4d301b7dcc49bfd71fbaeeff5581aebe",
+    "url": "/react-caffeSt-app_project/static/media/smothies.4d301b7d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00f7133ccedcebf486aca227da5431f7.js"
+  "/react-caffeSt-app_project/precache-manifest.029685f4b1add1ce6e5e5afeb05abb53.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-caffeSt-app_project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
